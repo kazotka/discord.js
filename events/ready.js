4f2615af58a4b4b573e1f1db819e19d8833ed314
kazotka <107224332+kazotka@users.noreply.github.com>
2022-07-12 14:31:47 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,19 +1,15 @@
 module.exports = {
-	// Appelle de l'événement.
 	name: 'ready',
-	// Exécuter seulement au lancement ou non.
 	once: true,
 	execute(client) {
-
-		// Appelle de la configuration, initialisation du status et message de lancement.
 
 		const dotenv = require('dotenv')
 		dotenv.config();
 
-    client.user.setStatus(process.env.status);
-    client.user.setActivity(process.env.display, { type: process.env.activity });
+    			client.user.setStatus(process.env.status);
+   			 client.user.setActivity(process.env.display, { type: process.env.activity });
 
-		console.log(`Toujours prêt.. !`);
+		console.log(`Ready for the show.. !`);
 
 	},
 };
